05fdf1f819a695ef6e4cb82d94df761a59650c6f
Agustin Molina <am.motos@hotmail.com>
2024-09-21 02:33:25 -0300

    Refactorizar componente de barra lateral para ajustar el ancho de la animación y agregar botón de inicio de sesión

--- src/lib/database/models/transaction.model.ts
+++ src/lib/database/models/transaction.model.ts
@@ -1,16 +1,5 @@
 import { Schema, model, models } from "mongoose";
 
-// Definición de la interfaz ITransaction que extiende Document
-export interface ITransaction extends Document {
-  createdAt: Date;
-  stripeId: string;
-  amount: number;
-  plan?: string;
-  credits?: number;
-  buyer: Schema.Types.ObjectId;
-}
-
-// Definición del esquema de Mongoose para el modelo Transaction
 const TransactionSchema = new Schema({
   createdAt: {
     type: Date,
@@ -37,7 +26,6 @@ const TransactionSchema = new Schema({
   },
 });
 
-// Creación del modelo Transaction basado en el esquema TransactionSchema
-const Transaction = models?.Transaction || model<ITransaction>("Transaction", TransactionSchema);
+const Transaction = models?.Transaction || model("Transaction", TransactionSchema);
 
 export default Transaction;
