8f43b3a74e4dac2da91c8f6a5c801ea9da3bd8db
Agustin <am.motos@hotmail.com>
2024-10-14 23:09:54 -0300

    feat: Comment out unused imports and functionality in customization page

--- src/app/(root)/customization/page.tsx
+++ src/app/(root)/customization/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { getUserById } from "@/lib/actions/user.actions";
 import { getUserLogos } from "@/lib/actions/logo.actions";
 import { auth } from "@clerk/nextjs/server";
-import CustomizationGallery from "@/components/shared/CustomizationGallery";
-import { getUserImages } from "@/lib/actions/image.actions";
+// import CustomizationGallery from "@/components/shared/CustomizationGallery";
+// import { getUserImages } from "@/lib/actions/image.actions";
 
 const page = async () => {
   const { userId } = auth();
@@ -14,12 +14,12 @@ const page = async () => {
 
   const user = await getUserById(userId);
   const logos = await getUserLogos({ userId: user._id });
-  const images = await getUserImages({userId: user._id});
+  // const images = await getUserImages({userId: user._id});
   
   
   return (
     <div>
-      <CustomizationGallery logos={logos?.data} images={images?.data} />
+      {/* <CustomizationGallery logos={logos?.data} images={images?.data} /> */}
     </div>
   );
 };
