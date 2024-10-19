d371c14a6814cb8b7fad9dc96a47249fbe776eb3
Agustin <am.motos@hotmail.com>
2024-10-19 03:37:20 -0300

    refactor: Enhance logo display with improved image handling and layout; define Image interface for better type safety

--- src/app/(root)/logos/page.tsx
+++ src/app/(root)/logos/page.tsx
@@ -2,12 +2,25 @@ import LogoForm from "@/components/shared/LogoForm";
 import { getUserLogos } from "@/lib/actions/logo.actions";
 import { getUserById } from "@/lib/actions/user.actions";
 import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
 import React from "react";
 
-const page = async () => {
+interface Image {
+  _id: string;
+  title: string;
+  publicId: string;
+  secureURL: string;
+  width: number;
+  height: number;
+  author: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
 
+const page = async () => {
   const { userId } = auth();
-  
+
   if (!userId) {
     return <div>Error: User ID is required</div>;
   }
@@ -20,15 +33,21 @@ const page = async () => {
       <LogoForm action="Add" userId={user._id} />
 
       <section>
-        <ul className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:gap-9 xl:grid-cols-3 max-w-7xl items-center mx-auto px-1">
-          {images?.data?.map((image: any) => (
+        <ul className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 md:gap-9 xl:grid-cols-4  items-center  px-1">
+          {images?.data?.map((image: Image) => (
             <li
               key={image.title}
-              className="w-full rounded-[16px] border-2 border-purple-200/20 p-8 shadow-xl shadow-purple-200/20"
+              className="w-full rounded-[16px] border-2 border-purple-200/20 p-2 shadow-xl shadow-purple-200/20"
             >
-              <div className="flex-start flex-col gap-3">
-                <p className="font-bold mt-2 text-black">{image.title}</p>
-                
+              <div className="flex flex-col gap-3">
+                <p className="font-bold mt-2">{image.title}</p>
+                <Image
+                  src={image.secureURL}
+                  alt={image.title}
+                  width={300}
+                  height={300}
+                  className="rounded-xl mx-auto"
+                />
               </div>
             </li>
           ))}
